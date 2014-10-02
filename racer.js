d510e3f172a5a1c7b8cce4083869db390e90546a
Justin Wagner <jwag789@gmail.com>
2014-10-02 19:33:13 -0400

    Add winner and loser state

--- racer.js
+++ racer.js
@@ -1,23 +1,34 @@
 var playerOnePos = 1;
-var PlayerTwoPos = 1;
+var playerTwoPos = 1;
+var game = true;
 
 var updatePlayerPosition = function(player, position){
   $('#player' + player + ' td').removeClass('active');
   $('#player' + player + ' td:eq(' + (position) + ')').addClass("active");
 }
 
+var checkWinner = function(player){
+  if ($('#player' + player + ' td:last-child').hasClass("active")){
+    game = false;
+  }
+}
+
 $(document).ready(function(){
 
-  (document).on('keyup', function(event){
-    
-    if(event.keyCode==81){
-      updatePlayerPosition(1, playerOnePos);
-      playerOnePos += 1;
-    }
+  $(document).on('keyup', function(event){
+    if (game===true){
+      
+      if(event.keyCode==81){
+        updatePlayerPosition(1, playerOnePos);
+        checkWinner(1);
+        playerOnePos += 1;
+      }
 
-    if(event.keyCode==80){
-      updatePlayerPosition(2, playerTwoPos);
-      playerTwoPos += 1;
+      if(event.keyCode==80){
+        updatePlayerPosition(2, playerTwoPos);
+        checkWinner(2);
+        playerTwoPos += 1;
+      }
     }
     
   })
